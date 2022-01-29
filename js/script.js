25ee456be9e48fdf45b60520047fd096929aa2fd
diyor <dierismailov9@gmail.com>
2022-01-29 14:44:46 +0500

    adap

--- js/script.js
+++ js/script.js
@@ -20,10 +20,40 @@ const swiper2 = new Swiper(".mySwiper2", {
     },
 });
 
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card'),
+      burgerBtn = document.querySelector('.burger-menu__btn '),
+      burgerClose = document.querySelector('.burger-menu__close'),
+      burgerMenu = document.querySelector('.menu__small-screen'),
+      body = document.querySelector('.body');
  
 
 cards.forEach(item => {
     let itemWidth = item.clientWidth;
     item.style.height = itemWidth + 'px';
+})
+
+window.addEventListener('resize', () => {
+  cards.forEach(item => {
+    let itemWidth = item.clientWidth;
+    item.style.height = itemWidth + 'px';
+})
+})
+
+
+
+burgerBtn.addEventListener('click', (e) => {
+e.preventDefault(); 
+burgerMenu.classList.add('show');
+body.classList.add('hidden')
+})
+burgerClose.addEventListener('click', (e) => {
+e.preventDefault(); 
+burgerMenu.classList.remove('show');
+body.classList.remove('hidden');
+})
+window.addEventListener('resize', () => {
+if(window.innerWidth > 991){
+burgerMenu.classList.remove('show');
+body.classList.remove('hidden');
+}
 })
